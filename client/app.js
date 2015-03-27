4d780db3d26c9816e0adb7a7464e42db364cd60f
BafS <fabacrans@hotmail.com>
2015-03-27 19:49:29 +0100

    add room part

--- client/app.js
+++ client/app.js
@@ -1,4 +1,14 @@
+
 if (Meteor.isClient) {
+	// Router.onBeforeAction(function() {
+	// 	if (! Meteor.userId()) {
+	// 		// this.render('login');
+	// 	} else {
+	// 		this.next();
+	// 	}
+	// });
+
+
 	// counter starts at 0
 	Session.setDefault('counter', 0);
 
@@ -8,8 +18,20 @@ if (Meteor.isClient) {
 		}
 	});
 
+
 	Router.route('/', function () {
 		this.render('welcomePage');
+	});
+
+	Router.route('/room/:uid', function () {
+		Template.room.helpers({
+			uid: this.params.uid
+		});
+		this.render('room');
+	});
+
+	Router.route('/room', function () {
+		this.render('no-room');
 	});
 
 	Router.map(function() {
@@ -19,4 +41,5 @@ if (Meteor.isClient) {
 	Router.map(function() {
 		this.route('peer');
 	});
+
 }
