8701fdaf6987c70f0b00377df0ca338c9d7b779b
BafS <fabacrans@hotmail.com>
2015-03-27 18:43:44 +0100

    first pages and style

--- client/app.js
+++ client/app.js
@@ -1,10 +1,22 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+	// counter starts at 0
+	Session.setDefault('counter', 0);
 
-  Template.body.helpers({
-    organisations: function () {
-      return Organisations.find({});
-    }
-  });
+	Template.body.helpers({
+		organisations: function () {
+			return Organisations.find({});
+		}
+	});
+
+	Router.route('/', function () {
+		this.render('welcomePage');
+	});
+
+	Router.map(function() {
+		this.route('organisation');
+	});
+
+	Router.map(function() {
+		this.route('peer');
+	});
 }
