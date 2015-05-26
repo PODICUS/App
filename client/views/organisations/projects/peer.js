800b3a34aee7236c17f9fb3b2dca2011f796ef11
Sacha Bron <sacha.bron@copperflake.com>
2015-05-26 10:16:53 +0200

    Sending parameters to asm.js

--- client/views/organisations/projects/peer.js
+++ client/views/organisations/projects/peer.js
@@ -1,9 +1,23 @@
 if (Meteor.isClient) {
-	var worker = new Worker('http://localhost:3000/cfs/files/asmjsFiles/LG8yh9psshjWXGuv2/pi.js');
-	
-	worker.onmessage = function(e) {
-		console.log('Message received from worker:', e);
-	}
+	Template.projectPeer.events({
+		'click #launch-computation': function (event, template) {
+			var worker = new Worker('http://localhost:3000/cfs/files/asmjsFiles/LG8yh9psshjWXGuv2/pi.js');
+			
+			worker.onmessage = function(e) {
+				console.log('Message received from worker:', e);
+			}
 
-	worker.postMessage({ arguments: [ "42", "23" ] });
+			var parameters = (typeof template.data.parameters !== 'undefined') ? template.data.parameters : [];
+
+			console.log("parameters: ", parameters);
+			
+			parameters = parameters.map(function (p) {
+				return math.eval(p.value, { x: 4 }).toString();
+			});
+
+			console.log("parameters: ", parameters);
+			
+			worker.postMessage({ arguments: parameters });
+		}
+	});
 }
