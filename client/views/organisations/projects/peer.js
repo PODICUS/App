d81c77e068d64f19086a0c5c726700f573a14209
Sacha Bron <sacha.bron@copperflake.com>
2015-05-27 11:39:42 +0200

    Itération de x

--- client/views/organisations/projects/peer.js
+++ client/views/organisations/projects/peer.js
@@ -2,6 +2,29 @@ if (Meteor.isClient) {
 	Template.projectPeer.events({
 		'click #launch-computation': function (event, template) {
 			console.log(template)
+
+			var progress = Progress.find({ projectId: template.data._id }).fetch()
+
+			function getX() {
+				var x = 0;
+
+				for (var i = 0, l = progress.length; i < l; i++) {
+					if (progress[i].status === 'incomplete') {
+						return progress[i].x;
+					}
+
+					var x = progress[i].x + 1;
+				}
+
+				return x;
+			}
+
+			var x = getX();
+			console.log('x:', x);
+
+			var progressId = Progress.insert({ projectId: template.data._id, x: x, status: 'running' });
+			console.log('progressId:', progressId);
+
 			var worker = new Worker('http://localhost:3000/cfs/files/asmjsFiles/5bAqiqpCDNKGoL8N2/pi.js');
 			var results = [];
 
@@ -12,8 +35,13 @@ if (Meteor.isClient) {
 				} else if(e.data.type === 'end') {
 					console.log('end!', results);
 					Projects.update({ _id: template.data._id }, { $push: {
-						results: { x: 4, values: results }
+						results: { x: x, values: results }
 					}});
+
+					Progress.update({ _id: progressId }, { $set: { status: 'done' }});
+					/*Projects.update({ _id: template.data._id, 'progress.id': progressId }, {
+						$set: { 'progress.status': 'done' }
+					});*/
 				}
 			}
 
@@ -22,12 +50,22 @@ if (Meteor.isClient) {
 			console.log("parameters: ", parameters);
 			
 			parameters = parameters.map(function (p) {
-				return math.eval(p.value, { x: 4 }).toString();
+				return math.eval(p.value, { x: x }).toString();
 			});
 
 			console.log("parameters: ", parameters);
 			
 			worker.postMessage({ arguments: parameters });
+		}
+	});
+
+
+	Template.body.events({
+		'beforeunload': function (event, template) {
+			console.log(event, template);
+			alert(1);
+			event.preventDefault();
+			return;
 		}
 	});
 }
